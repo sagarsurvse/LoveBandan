77f105b682eaa430a1c3299e0e23bade43a6758c
Mungu Media <mungumedia.developer@gmail.com>
2022-07-31 20:32:31 +0530

    first ajay project

---
+++ app/src/main/java/com/app/lovebandhan/SliderData.java
@@ -0,0 +1,21 @@
+package com.app.lovebandhan;
+
+public class SliderData {
+    private String imgUrl;
+
+
+    // Constructor method.
+    public SliderData(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+
+}
