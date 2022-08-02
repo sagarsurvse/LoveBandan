1fbf5b9b0e4e5fe0568359f96af7fe14a6884834
Mungu Media <mungumedia.developer@gmail.com>
2022-08-02 21:20:11 +0530

    commit 02 aus     1) design new start screen     2) verify otp screen     3) step one form screen

---
+++ app/src/main/java/com/app/lovebandhan/Screen/VerifyScreen.java
@@ -0,0 +1,16 @@
+package com.app.lovebandhan.Screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.lovebandhan.R;
+
+public class VerifyScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_screen);
+    }
+}
