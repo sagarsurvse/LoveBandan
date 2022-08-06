b24498982546b578ebc5a414994df6e7f77218a1
Mungu Media <mungumedia.developer@gmail.com>
2022-08-06 12:38:43 +0530

    commit 02 aus     created form two step

---
+++ app/src/main/java/com/app/lovebandhan/Screen/FormStep/FormStepTwo.java
@@ -0,0 +1,16 @@
+package com.app.lovebandhan.Screen.FormStep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.lovebandhan.R;
+
+public class FormStepTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_step_two);
+    }
+}
