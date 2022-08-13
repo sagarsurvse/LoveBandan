64c1e981623ad484e72f85939fbac7544b6e0405
Ajay <ajayingle549@gmail.com>
2022-08-13 12:55:56 +0530

    13 augest

--- app/src/main/java/com/app/lovebandhan/Screen/FormStep/FormStepThree.java
+++ app/src/main/java/com/app/lovebandhan/Screen/FormStep/FormStepThree.java
@@ -24,11 +24,19 @@ public class FormStepThree extends AppCompatActivity {
     ArrayAdapter<CharSequence> adapterSpinCommunity;
 
 
-    String[] liveArray = {"live1", "live2","live3"};
-    String[] meterialarray = {"m1", "m2"};
-    String[] dietArray = {"d1", "d2","d3","d4","d5"};
-    String[] heightarray = {"a1", "a2","a3","a4"};
-    String[] subcommunity = {"c1", "c2","c3","c4"};
+    String[] liveArray = {"Aadhra Pradesh","Bihar","dehli-NCR","Gujrat","Haryana","Jharkhand","Kaenataka","Kerala","Madhya Pradesh","Maharastra","Orissa","Punjab","Rajsthan",
+                          "Tamil Nadu","Uttar Pradesh","Weat Bengal","Sikkim","Uttaranchal","Tripura"};
+    String[] meterialarray = {"Never Married", "Divorcrd","Windowed","Awaiting Divorce","Annulled"};
+    String[] dietArray = {"Veg", "Non-Veg","Occasionally Non-Veg","Eggetarian","Jain","Vegan"};
+    String[] heightarray = {"4ft 5in-134cm", "4ft 6in-137","4ft 7in-139cm","4ft 8in-142cm","4ft 9in-144cm","4ft 10in-147cm","4ft 11in-149cm","5ft- 152cm","5ft 1in-154",
+                             "5ft 2in-157cm","5ft 3in-160cm","5ft 4in -162cm","5ft 5in-165cm","5ft 6in-167cm","5ft 7in-170cm","5ft 8in-172cm","5ft 9in-175cm","5ft 10in-177cm",
+                             "5ft 11in-180cm","6ft - 182cm","6ft 1in-185cm","6ft 2in-187cm","6ft 3in-190cm","6ft 4in-193cm","6ft 5in-195cm","6ft 6in-198cm","6ft 7in-200cm",
+                             "6ft 8in-203cm","6ft 9in-205cm","6ft 10in-208cm","6ft 11in-210cm","7ft- 213cm"};
+    String[] subcommunity = {"Agarwal","Aroya","Baniya","Brahmin-Bhumihar","Brahmin-Gour","Brahmin-Kanyakubja","Brahmin-Other","Brahmin-Saryuparin","Gupta","Jat","Jatav","Kayastha","Khatri",
+                             "Kshatriya","Kurmi","Kushwaha","Rajput","Scheduled Caste(SC)","Thakur","Yadav","24 Manai Telugu Chettier","96 Kuli Maratha","96K Kokanastha","Adhi Andhra","Adi Dharmi","Adi Dravida",
+                              "Adi Karnataka","Agamudayar","Agarwal","Agnikula Kshatriya","Agri","Ahir","Ahom","Ambalavasi","Arcot","Arekatica","Arora","Arunthathiyar","Arya Vysya","Aryasamaj","Ayyaraka",
+                               "Badaga","Baghel/Pal/Gaderiya","Bahi","Baidya","Baishya","Bajantri","Balija","Balija-Naidu","Banayat Oriya","Banik","Baniya","Barai","Bari","Barnwal",
+                               "Barujibi","Besta","Bhandari","Bhatia","Bhatraju","Bhavsar","Bhovi","Billava","Boya/Nayak/Naik","Boyer","Brahmatt","Brahmin-Anavil","B"};
 
 
     @Override
@@ -55,7 +63,7 @@ public class FormStepThree extends AppCompatActivity {
         SpinSubCommunity = findViewById(R.id.spinner_sub);
 
 
-        // for profile
+        // for profilea
         adapterSpinlive =    new ArrayAdapter<CharSequence>(this,android.R.layout.simple_spinner_item,liveArray);
         adapterSpinlive.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         SpinLive.setAdapter(adapterSpinlive);
