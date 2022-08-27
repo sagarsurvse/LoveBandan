2c01e43035d5a3d4e60dea0a2e1ae1afdda8354f
Ajay <ajayingle549@gmail.com>
2022-08-27 17:15:12 +0530

    25 augest

---
+++ app/src/main/java/com/app/lovebandhan/Screen/User_detailspage.java
@@ -0,0 +1,16 @@
+package com.app.lovebandhan.Screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.lovebandhan.R;
+
+public class User_detailspage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_detailspage);
+    }
+}
